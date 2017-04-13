16f3bf3b3fcbfbdaeacfb999cc4608feb3c57cd3
Karpov Babu <karpovbabu.v@gmail.com>
2017-04-13 12:58:19 +0530

    Initial Commit

---
+++ src/main/java/com/megatech/KendoFrontApplication.java
@@ -0,0 +1,25 @@
+package com.megatech;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication(scanBasePackages = { "com.megatech" })
+public class KendoFrontApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return configureApplication(builder);
+	}
+
+	public static void main(String[] args) {
+		configureApplication(new SpringApplicationBuilder()).run(args);
+	}
+
+	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder.sources(KendoFrontApplication.class).bannerMode(Banner.Mode.OFF);
+	}
+
+}
